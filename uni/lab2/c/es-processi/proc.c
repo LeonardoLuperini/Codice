2d7bd59d60c76df166ef52f9b1b4c748048ee07f
LeonardoLuperini <leonardo.luperini.29@gmail.com>
2023-03-13 00:44:23 +0100

    Mdificato proc.c

--- uni/lab2/c/es-processi/proc.c
+++ uni/lab2/c/es-processi/proc.c
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define C 7
+#define C 50
 #define EXIT_ERROR(val, errval, msg) if ((val) == (errval)) { perror((msg)); exit(EXIT_FAILURE);}
 #define SON(pid) (pid == 0)
 #define SHM_NAME "/shmlluperini5"
@@ -21,13 +21,9 @@ int main(void) {
 	sem_t* sem;
 	size_t* ptr_counter;
 
-	int fd = shm_open(SHM_NAME, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
-	EXIT_ERROR(fd, -1, "Error shm_open");
-	EXIT_ERROR(ftruncate(fd, sizeof(size_t)), -1, "Error ftruncate");
-	ptr_counter = mmap(NULL, sizeof(size_t), PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
-	EXIT_ERROR(sem, MAP_FAILED, "Error mmap");
+	ptr_counter = mmap(NULL, sizeof(size_t), PROT_WRITE|PROT_READ, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+	EXIT_ERROR(ptr_counter, MAP_FAILED, "Error mmap");
 	(*ptr_counter) = 0;
-	close(fd);
 
 	sem = mmap(NULL, sizeof(sem_t),
 			PROT_READ|PROT_WRITE,
@@ -39,11 +35,11 @@ int main(void) {
 		pid = fork();
 		EXIT_ERROR(pid, -1, "Errore Fork");
 		if SON(pid) {
-			sleep(rand()%6);
+			//sleep(rand()%6);
 			fprintf(stderr, "%d: Partito\n", getpid());
 			sem_wait(sem);
 			fprintf(stderr, "%d: HO IL POTEREEEE!\n", getpid());
-			sleep(rand()%3);
+			//sleep(rand()%3);
 			fprintf(stderr, "%d: Incremento il counter a %lu\n", getpid(), ++(*ptr_counter));
 			sem_post(sem);
 			return EXIT_SUCCESS;
