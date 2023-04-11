6a9768f0052020b868d3c7d3098371738c1fcb0d
LeonardoLuperini <leonardo.luperini.29@gmail.com>
2023-04-11 14:51:18 +0200

    Added macros to err handling lib

--- uni/lab2/c/lib/error_handling_utils.h
+++ uni/lab2/c/lib/error_handling_utils.h
@@ -3,14 +3,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ERR_EXIT(cond, msg) 				\
-if ((cond)) { 								\
-	fprintf(stderr, msg); 					\
-	exit(EXIT_FAILURE); 					\
-}
+#define ERR_PEXIT(cond, msg) 												   \
+	if ((cond)) { 															   \
+		fprintf(stderr, msg); 												   \
+		exit(EXIT_FAILURE); 												   \
+	}
 
-#define ERR_RET(cond, retval) 				\
-if ((cond)) return retval;
+#define ERR_RET(cond, retval) 												   \
+	if ((cond)) return retval;
 
+
+#define ERR_FREE_FREE(cond, ptr, msg)                                          \
+    if ((cond)) {                                                              \
+        perror((msg));                                                         \
+        free((ptr));                                                           \
+        exit(EXIT_FAILURE);                                                    \
+    }
+
+#define EXIT_ERR(cond, msg)                                                    \
+    if ((cond)) {                                                              \
+        perror((msg));                                                         \
+        exit(EXIT_FAILURE);                                                    \
+    }
 
 #endif
